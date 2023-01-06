3c0a9e6b0d254f39061f02514bcd5aeb7ef08004
jasonl <jasonl@bitheads.com>
2023-01-06 18:45:36 -0500

    wrap table with overflow

--- src/theme/MDXComponents/index.js
+++ src/theme/MDXComponents/index.js
@@ -38,5 +38,14 @@ const MDXComponents = {
   PartialServop: PartialServop,
   DocCardList: DocCardList,
   LiteYouTubeEmbed: LiteYouTubeEmbed,
+  table: ({ children, ...props }) => {
+    const tableHeadings = children[0].props.children.props.children;
+    const hasTheadValue = !Array.isArray(tableHeadings) || tableHeadings.every(({ props }) => props.children);
+    return (
+      <div className="table-wrapper">
+        <table {...props} children={hasTheadValue ? children : children.slice(1)} />
+      </div>
+    );
+  },
 };
 export default MDXComponents;
